836b9be28541cd9e24a75f63583556b6ee90d85a
Mathew Leriche <ml529@njit.edu>
2019-09-17 01:08:29 -0400

    Testing config details

--- initDB.php
+++ initDB.php
@@ -5,4 +5,16 @@
 require('conf.php');
 echo "Loaded host: " . $host;
 
+
+//new lines below 
+try{
+	$conn_string = "mysql:host=$host;dbname=$databasename;charset=utf8mb4";
+	$db = new PDO($conn_string, $username, $password);
+	echo "Connected";
+}
+
+catch(Exception $e){
+	echo $e->getMessage();
+	echo "Something went wrong";
+}
 ?>
