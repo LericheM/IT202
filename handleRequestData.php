b5c62d9a906d249c07556ceab38dcccfe86842eb
Mathew Leriche <ml529@njit.edu>
2019-09-26 10:50:16 -0400

    Attempting to add changes for Homework assignment 1

--- handleRequestData.php
+++ handleRequestData.php
@@ -8,4 +8,14 @@ if(isset($_GET['number'])){
 	echo "<br>" . $number . " should be a number...";
 	echo "<br>but it might not be<br>";
 }
+if(isset($_GET['add1'])){
+	$add1 = $_GET['add1'];
+}}
+if(isset($_GET['add2'])){
+	$add2 = $_GET['add2'];
+}
+if($add1 != NULL and $add2 != NULL){
+	$sum = $add1+$add2;
+	echo "<br> The sum of " .$add1." and ".$add2." is: ". $sum;
+}
 ?>
