2f35ac7fe3c94836a07f487db87cc7252a30d3de
Mathew Leriche <ml529@njit.edu>
2019-09-26 10:34:16 -0400

    Using conditionals w/ GET requests

--- handleRequestData.php
+++ handleRequestData.php
@@ -3,4 +3,9 @@ echo "<pre>" . var_export($_GET, true) . "</pre>";
 if(isset($_GET['name'])){
 	echo "<br> Hello, " . $_GET['name'] . "<br>";
 }
+if(isset($_GET['number'])){
+	$number = $_GET['number'];
+	echo "<br>" . $number . " should be a number...";
+	echo "<br>but it might not be<br>";
+}
 ?>
