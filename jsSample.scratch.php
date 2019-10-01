c511da056197bfef7ad8f9bd135003ea684d7b07
Mathew Leriche <ml529@njit.edu>
2019-10-01 11:03:57 -0400

    Using functions, and console cmds in JS

--- jsSample.scratch.php
+++ jsSample.scratch.php
@@ -2,11 +2,28 @@
 <head>
 <!--css and js here -->
 <script>
-alert("Hello World");
+function pageLoaded(){
+
+//alert("Hello World");
+var myVariable;//scoping type weird here
+let myIntVariable;//only works in this scope
+//myVariable = prompt("What's your name?");
+//alert("Hello,"+ myVariable);
+
+let myNum = 0;
+for(let i = 0; i < 10; i++){
+	myNum += 0.1;
+}
+//alert("My num is 1: " + (myNum == 1) + "/nMy Num: " + myNum);
+console.log("My num is 1: " +(myNum ==1) +"/nMy num: " +myNum);
+
+	//let myP = document.getElementById("myP");//define elements before calling
+	console.log(myP);
+}
 </script>
 </head>
-<body>
+<body onload = "pageLoaded();">
 <!-- html content -->
-<p> It loaded, yay!</p>
+<p id= "myP"> It loaded, yay!</p>
 </body>
 </html>
