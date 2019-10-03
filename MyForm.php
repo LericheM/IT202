5a13d009f3ca208181d9a9e33211e669a4d31c7d
Mathew Leriche <ml529@njit.edu>
2019-10-03 10:50:21 -0400

    Added a php comment

--- MyForm.php
+++ MyForm.php
@@ -1,4 +1,8 @@
 <?php
+//some quick error handling
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(E_ALL);
 function getName(){
     if(isset($_GET['name'])){
         echo "<p>Hello, " . $_GET['name'] . "</p>";
