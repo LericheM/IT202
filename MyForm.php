5b4ef869f41cf2eb66ac91dad61b8692e8ddb786
Mathew Leriche <ml529@njit.edu>
2019-10-03 11:02:29 -0400

    Testing different types of elements for forms

--- MyForm.php
+++ MyForm.php
@@ -1,5 +1,4 @@
 <?php
-//some quick error handling
 ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(E_ALL);
@@ -15,7 +14,22 @@ function getName(){
 <body><?php getName();?>
 <form mode="GET" action="#">
 <input name = "name" type = "text" placeholder = "Enter your name"/>
-<input type="submit" value= "Confirm"/>
+<input name = "number" type = "number" placeholder ="Enter a number"/>
+<input name = "passwd" type = "password" placeholder= "Enter a fake password"/>
+
+<label for="Yes">Yes</label>
+<input type="radio" name="radio" id="No" value="No"/>
+
+<select name="dropdown">
+    <option value="1">One</option> 
+    <option value="2">Two</option>    
+    <option value="3">Three</option>
+</select>
+<input type="checkbox" name="check"/>
+<textarea name="text"></textarea>
+<input type="date" name="date"/>
+
+<input type="submit" value="Try it"/>
 </form>
 </body>
 </html>
