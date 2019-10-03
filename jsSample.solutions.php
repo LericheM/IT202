12f5ef90b2c69429d85b56043301cd48a86de414
Mathew Leriche <ml529@njit.edu>
2019-10-02 23:03:15 -0400

    Learning to use DOM objects in JS

---
+++ jsSample.solutions.php
@@ -0,0 +1,20 @@
+
+<html>
+<head>
+
+</head>
+<body>
+    //Using help from w3schools.com tutorial on DOM Nodes
+    <div id = "div1">
+        <p id = 'first'>Mathew</p>
+        <p id = 'last'>Leriche</p>
+    </div>
+
+    <script>
+        var div2 = document.createElement("div");
+        var node = document.createTextMode("<p id = 'text'> new element added</p>");
+        div2.appendChild(node);
+    </script>
+</body>
+
+</html>
