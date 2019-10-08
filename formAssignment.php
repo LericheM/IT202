4b277b90b806c69acdc95f8578ed24145f98bafa
LericheM <ml529@njit.edu>
2019-10-07 23:49:57 -0400

    populated formAssignment.php

--- formAssignment.php
+++ formAssignment.php
@@ -11,23 +11,45 @@ function getName(){
 <html>
 <head></head>
 <body><?php getName();?>
-<form mode="GET" action="#">
-<input name="name" type="text" placeholder="Enter your name"/>
-<!-- add password field-->
-<input type="password" name="password1" 
-placeholder="Enter your password">
-<!-- add confirm password field-->
-<input type="password" name="password2"
-placeholder="Please confirm your passsword">
+<form name= "login" mode="POST" onsubmit= "return validateForm()">
+	<input name="username" type="text" 
+	placeholder="Enter your name" required/>
+	<!-- add password field-->
+	<input type="password" name="password" 
+	placeholder="Enter your password" required>
+	<!-- add confirm password field-->
+	<input type="password" name="passwordConfirm"
+	placeholder="Please confirm your passsword" required>
+	
 
-<!-- ensure passwords match before sending the form
-		AND/OR
-	validate password matches confirmation on php side
--->
-<!-- change form submit type to post, adjust php checks for change in type-->
+	<!-- ensure passwords match before sending the form
+			AND/OR
+		validate password matches confirmation on php side
+	-->
+	<!-- change form submit type to post, adjust php checks for change in type-->
 
-<input type="submit" value="Try it"/>
+	<input type="submit" value="Submit"/>
+	<p id ='after'></p>
 </form>
+	<script>
+		function validateForm(){
+			var uname = document.forms["login"]["username"].value;
+			var password = document.forms["login"]["password"].value;
+			var pass2 = document.forms["login"]["passwordConfirm"].value;
+			if (password != pass2) {
+				alert("Passwords must match!");
+				return false;
+			}
+			else{
+				output = "";
+				for(i = 0;i < password.length; i++){
+					output += "*"
+				}
+				document.getElementById("after").innerHTML = "You entered: U: "+uname+"\n P:"
+				+ output;
+			}
+		}
+	</script>
 </body>
 </html>
 
