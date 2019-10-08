0661d3214632adfd52cfc9ac404d96b5e1f01079
LericheM <ml529@njit.edu>
2019-10-08 10:48:49 -0400

    Adding email validation and basic css styling.

--- formAssignment.php
+++ formAssignment.php
@@ -36,7 +36,10 @@ function checkPasswords(){
 	<!-- add confirm password field-->
 	<input type="password" name="passwordConfirm"
 	placeholder="Please confirm your passsword" required>
-	
+	<!-- EMAIL -->
+	<input name="email" type="emai" placeholder="name@example.com"/>
+	<span id="validation.email" style="display:none;"></span>
+
 
 	<!-- ensure passwords match before sending the form
 			AND/OR
@@ -51,14 +54,28 @@ function checkPasswords(){
 			var uname = document.forms["login"]["username"].value;
 			var password = document.forms["login"]["password"].value;
 			var pass2 = document.forms["login"]["passwordConfirm"].value;
+			var email = form.email.value;
+			var ev = document.getElemenetById("validation.email");
+			let succeeded = true;
+			if(email.indexOf('@') > -1){
+				ev.style.display = "none";
+			}
+			else{
+				ev.style.display = "block";
+				ev.innerText = "Please enter a valid email address";
+				succeeded = false;
+			}
 			if (password != pass2) {
 				alert("Passwords must match!");
 				return false;
 			}
 			console.log(uname)
 			console.log(password)
+			return succeeded
 		}
 	</script>
+		<input name="email" type="emai" placeholder="name@example.com"/>
+		<span id="validation.email" style="display:none;"></span>
 </body>
 </html>
 <?php checkPasswords();?>
