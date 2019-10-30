8e21c0482b3076a7fb8d0e1fc9a36db0ec4a454f
LericheM <ml529@njit.edu>
2019-10-30 13:31:58 -0400

    adding backtics where they belong

--- backend_login.php
+++ backend_login.php
@@ -10,11 +10,11 @@
 
     try {
         $db = new PDO($conn_string,$username,$password);
-        $query = "create table if not exists 'Players'(
-            'id' int auto_increment not null,
-            'username' varchar(30) not null unique,
-            'pin' int default 0,
-            PRIMARY KEY ('id')
+        $query = "create table if not exists `Players`(
+            `id` int auto_increment not null,
+            `username` varchar(30) not null unique,
+            `pin` int default 0,
+            PRIMARY KEY (`id`)
         ) CHARACTER SET utf8 COLLATE utf8_general_ci";
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
         $stmt = $db->prepare($query);
@@ -22,18 +22,6 @@
         $r = $stmt->execute();
         echo "<br>". ($r>0?"Created table or already exists":"Failed to create table")."<br>";
         unset($r);
-
-        if(isset($_POST["user"])){
-            $user = $_POST["user"];
-            $flag1 = true;
-        }
-        if(isset($_POST["pass"])){
-            $pass = $_POST["pass"];
-            $flag2 = true;
-        }
-        if($flag1 and $flag2){
-            lookup($user,$pass);
-        }
     } catch (Exception $e) {
         $response = "DB Error: " . $e;
         }
