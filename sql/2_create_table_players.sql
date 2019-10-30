6f23a75d72b3a7ad1a9647d57bac981da423317e
LericheM <ml529@njit.edu>
2019-10-30 16:16:22 -0400

    last change to table creation

--- sql/2_create_table_players.sql
+++ sql/2_create_table_players.sql
@@ -2,5 +2,6 @@ create table if not exists `Players`(
             `id` int auto_increment not null,
             `username` varchar(30) not null unique,
             `pin` int default 0,
+            `password` varchar(16),
             PRIMARY KEY (`id`)
         ) CHARACTER SET utf8 COLLATE utf8_general_ci
