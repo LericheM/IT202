49cf7f6b313281f19848da0b797c82febe555555
LericheM <ml529@njit.edu>
2019-10-31 15:20:44 -0400

    adding inits to flags

--- login.php
+++ login.php
@@ -3,6 +3,8 @@ function get_sample_users(){
 	ini_set('display_errors',1);
 	ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
+    $flag = false;
+    $flag2 = false;
     if(isset($_POST["username"])){
         $user_input = $_POST['username'];
         $flag = true;
