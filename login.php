a3dddfae6075896730b0068769f2b18d4cce2877
LericheM <ml529@njit.edu>
2019-10-31 16:11:05 -0400

    binding inputs

--- login.php
+++ login.php
@@ -46,9 +46,6 @@ function get_sample_users(){
 ?>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login</title>
     
 </head>
