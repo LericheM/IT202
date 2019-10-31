352bd342cd76f852c60873ca7f9be1966f8413b5
LericheM <ml529@njit.edu>
2019-10-31 15:40:09 -0400

    trying to change form to not refresh on submit

--- login.php
+++ login.php
@@ -3,19 +3,9 @@ function get_sample_users(){
 	ini_set('display_errors',1);
 	ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
-    $flag = false;
-    $flag2 = false;
-    if(isset($_POST["username"])){
-        $user_input = $_POST['username'];
-        $flag = true;
-    }
-    if(isset($_POST["pass"])){
-        $user_pass = $_POST['pass'];
-        $flag2 = true;
-    }
+
 	require('conf.php');
     $conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
-    if($flag & $flag2){
 	try{
 		$db = new PDO($conn_string, $username, $password);
         $select_query = "select username,token 
@@ -24,24 +14,24 @@ function get_sample_users(){
         and username = :usernamePlace";
 		$stmt = $db->prepare($select_query);
 		$r = $stmt->execute([":usernamePlace"=>$user_input,":passwdPlace"=>$user_pass]);
-        $results = $stmt->fetch(PDO::FETCH_ASSOC);
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
         if($stmt->errorInfo()){
             print_r($stmt->errorInfo());
         }
         else{
             if($results["password"] == $_POST['pass']){
-                return "Welcome back ". $user_input;
+                echo "Welcome back ". $user_input;
             }
             else{
-                return "User or password is incorrect";
+                echo "User or password is incorrect";
             }
         }
 	}
 	catch(Exception $e){
         $response = "DB Error: " . $e;
-        return $response;
+        echo $response;
 	}
-}
+
 
 
 }
@@ -60,7 +50,7 @@ function get_sample_users(){
     <div id = "screenOut">
             <p></p>
     </div>
-<form action="#" method="post">
+<form action="#" onsubmit="return false" method="post">
 <input type="text" name="username">
 <input type="password" name="pass" >
 <input type="submit" value="Login">
