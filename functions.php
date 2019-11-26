0b1762a4c2766b939bfdc0441399f98655be3015
LericheM <mleriche217@gmail.com>
2019-11-25 22:04:53 -0500

    attempting to re-implement multiple features

--- functions.php
+++ functions.php
@@ -2,10 +2,18 @@
 //Borrowed helper function structure from Toegel
 function get_username(){
     if($_SESSION['user']){
-        echo $_SESSION['user']['name'];
+        return $_SESSION['user']['name'];
     }
-    else{
-        echo "Session Missing!"
+}
+function check_sesh(){
+    return isset($_SESSION);
+}
+function fetch_id(){
+    if($_SESSION['user']){
+        return $_SESSION['user']['id'];
     }
+}
+if(isset($_POST["destination"])){
+    
 }
 ?>
