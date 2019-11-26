31896699c293f945912e75afec16bf6378c7948b
LericheM <mleriche217@gmail.com>
2019-11-25 22:25:21 -0500

    logging error sfor sessions

--- landing.php
+++ landing.php
@@ -5,6 +5,10 @@ include_once("./functions.php");
 <html>
 <head>
     <section id= "top">
+    <?php
+    session_start();
+    echo "We found: " . var_export($_SESSION['user'], true);
+    ?>
         Hey there <?php get_username();?>!
     </section>
 
