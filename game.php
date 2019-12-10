4e710c07cc130003303ba7f36680e5e3d8c52bd4
LericheM <ml529@njit.edu>
2019-12-10 11:01:21 -0500

    caching changes

--- game.php
+++ game.php
@@ -1,11 +1,14 @@
 <?php session_start();
+header('Cache-Control: no-cache, no-store, must-revalidate');
+header('Pragma: no-cache');
+header('Expires: 0');
 ?>
 <!DOCTYPE html>
 <html>
     <head>
-    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+    <!-- <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
     <meta http-equiv="Pragma" content="no-cache" />
-    <meta http-equiv="Expires" content="0" />
+    <meta http-equiv="Expires" content="0" /> -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="./style.css" rel="stylesheet" type="text/css">
     <script defer src="./main.js"></script>
