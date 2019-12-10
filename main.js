1a1c96af14cac19f5658f2e6880e03613d431945
LericheM <mleriche217@gmail.com>
2019-12-10 02:48:36 -0500

    starting ajax calls

--- main.js
+++ main.js
@@ -1,74 +1,84 @@
 //  Variables
-  let board;
-  let turn = 'x';
-  let win;
+let board;
+let turn = 'x';
+let win;
 
-  // Constants
-  const squares = Array.from(document.querySelectorAll('#board div'));
-  const messages = document.querySelector('h2');
-  const winningCombos = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6], 
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-    ];
-  // Event Listeners
-  document.getElementById('board').addEventListener('click', handleTurn);
-  document.getElementById('next-game-button').addEventListener('click', initBoard);
-  document.getElementById('Logout').addEventListener('click',logout);
+// Constants
+const squares = Array.from(document.querySelectorAll('#board div'));
+const messages = document.querySelector('h2');
+const winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6], 
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+  ];
+// Event Listeners
+document.getElementById('board').addEventListener('click', handleTurn);
+document.getElementById('next-game-button').addEventListener('click', initBoard);
+document.getElementById('Logout').addEventListener('click',logout);
 
 
-  // Functions
-  
-  function initBoard(){
-    board = [
-      "","","",
-      "","","",
-      "","",""
-    ];//layout is more clear in this manner
-    render();
-  }
-  initBoard();
+// Functions
 
-  function getWinner() {
-    let winner = null;
-    winningCombos.forEach((combo, index) => {
+function initBoard(){
+  board = [
+    "","","",
+    "","","",
+    "","",""
+  ];//layout is more clear in this manner
+  render();
+}
+initBoard();
 
-        if (board[combo[0]] && board[combo[0]] === board[combo[1]] 
-            && board[combo[0]] === board[combo[2]]) {
+function getWinner() {
+  let winner = null;
+  winningCombos.forEach((combo, index) => {
 
-            winner = board[combo[0]];
-        }
+      if (board[combo[0]] && board[combo[0]] === board[combo[1]] 
+          && board[combo[0]] === board[combo[2]]) {
+
+          winner = board[combo[0]];
+      }
 });
 
-    return winner ? winner : board.includes('') ? null : 'T';
+  return winner ? winner : board.includes('') ? null : 'T';
 };
 
 
-  function render(){
-    board.forEach(function(mark,index){
-      squares[index].textContent = mark ;``
-    });
-    messages.textContent = win === 'T' ? `Tie!` : win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
-  }
+function render(){
+  board.forEach(function(mark,index){
+    squares[index].textContent = mark ;``
+  });
+  messages.textContent = win === 'T' ? `Tie!` : win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
+}
 
-  
-  function handleTurn(event){
-    let idx = squares.findIndex(function(square){
-      return square === event.target;
-    });
-    board[idx] = turn;
 
-    turn = turn === 'x' ? 'o' : 'x';
+function handleTurn(event){
+  let idx = squares.findIndex(function(square){
+    return square === event.target;
+  });
+  board[idx] = turn;
 
-    win = getWinner();
+  turn = turn === 'x' ? 'o' : 'x';
 
-    console.log(board);
-    render();
-  }
+  win = getWinner();
 
+  console.log(board);
+  render();
+}
+
+function sendBoard(){
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+      board = this.responseText;
+    }
+  };
+  xhttp.open("POST","game.php",true);
+  xhttp.send();
+}
 
